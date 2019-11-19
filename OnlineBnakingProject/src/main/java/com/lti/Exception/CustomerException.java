defee073ba2cf22c36a9695dc409d66ee0db4f97
soum-pixel <soumyashenoy0205@gmail.com>
2019-11-19 15:47:39 +0530

    New Spring MVC Project Created

---
+++ OnlineBnakingProject/src/main/java/com/lti/Exception/CustomerException.java
@@ -0,0 +1,38 @@
+package com.lti.Exception;
+
+/**
+ * @author Jyoti Dixit 
+ * @Date 14/11/19
+ */
+
+public class CustomerException extends Exception
+{
+
+	public CustomerException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
